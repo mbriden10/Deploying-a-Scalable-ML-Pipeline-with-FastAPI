cf8ecda0b96c1abbd3b73356417274226d560cc4
Melissa Briden <mbriden759@gmail.com>
2026-01-01 19:40:14 +0000

    modified main to troubleshoot error with post

--- main.py
+++ main.py
@@ -45,30 +45,35 @@ async def get_root():
 # TODO: create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
+    try:
     # DO NOT MODIFY: turn the Pydantic model into a dict.
-    data_dict = data.dict()
+        data_dict = data.dict()
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
     # The data has names with hyphens and Python does not allow those as variable names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
-    data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
-    data = pd.DataFrame.from_dict(data)
+        data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
+        data = pd.DataFrame.from_dict(data)
 
-    cat_features = [
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "native-country",
-    ]
-    data_processed, _, _, _ = process_data(
-        data,
-        categorical_features=cat_features,
-        training=False,
-        encoder=encoder,
-        lb= None
-    )
-    _inference = inference(model, data_processed)[0]
-    return {"result": apply_label(_inference)}
+        cat_features = [
+            "workclass",
+            "education",
+            "marital-status",
+            "occupation",
+            "relationship",
+            "race",
+            "sex",
+            "native-country",
+        ]
+        data_processed, _, _, _ = process_data(
+            data,
+            categorical_features=cat_features,
+            training=False,
+            encoder=encoder,
+            lb= None
+        )
+        _inference = inference(model, data_processed)[0]
+        return {"result": apply_label(_inference)}
+    
+    except Exception as e:
+        return {"error": str(e)}
+    
